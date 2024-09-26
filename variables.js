fee5146878675ab686098bb6ffa2702345542847
Nas88888 <143430846+Nas88888@users.noreply.github.com>
2024-09-26 13:51:32 +0000

    trtd

--- variables.js
+++ variables.js
@@ -1,13 +1,13 @@
 // 1. Create a variable called `color` and assign it a string value of a color you like.
-
+let color = "red";
 
 // 2. Create a variable called `animal` and assign it a string value of an animal you like.
-
+let animal = "tiger";
 
 // 3. Create a variable called `creature`. 
 // - The calue should be the concatenation of the variable `color` and `animal`.
 // - Include a space!
-
+let creature = (color + "" + aniaml);
 
 
 
